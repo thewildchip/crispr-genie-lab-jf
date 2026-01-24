ffd76b1db8b18b5b2c6f51168e5e2bffc2279bd2
Daniel Panoor <fn.danpan@gmail.com>
2026-01-24 14:59:56 +0100

    new paths to off_target_old

--- src/mypackage/config.py
+++ src/mypackage/config.py
@@ -8,10 +8,12 @@ DATA_RAW = BASE_DIR / "data/raw"
 DATA_PROCESSED = BASE_DIR / "data/processed"
 DATA_PROCESSED_EFFICACY = DATA_PROCESSED / "efficacy"
 DATA_PROCESSED_OFFTARGET = DATA_PROCESSED / "off_target"
+DATA_PROCESSED_OFFTARGET_OLD = DATA_PROCESSED / "off_target_old"
 
 # Model paths
 MODELS_EFFICACY = BASE_DIR / "models/efficacy"
 MODELS_OFFTARGET = BASE_DIR / "models/off_target"
+MODELS_OFFTARGET_OLD = BASE_DIR / "models/off_target_old"
 
 # Notebook paths (optional)
 NOTEBOOKS_EFFICACY = BASE_DIR / "notebooks/efficacy"
